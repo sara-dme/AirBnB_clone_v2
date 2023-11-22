8696b60fbb3b40f5815f0e087d0f2ed9ea1a2b9a
aicha652 <aichaazlf652@gmail.com>
2023-11-22 08:33:10 -0800

    fix task 9 and 10

--- models/place.py
+++ models/place.py
@@ -9,6 +9,14 @@ from os import getenv
 
 storage_type = getenv("HBNB_TYPE_STORAGE")
 
+amenity_place = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True, nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True, nullable=False))
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
--- models/review.py
+++ models/review.py
@@ -10,8 +10,8 @@ class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        place_id = Column(String(60), Foreign_key('places.id'), nullable=False)
-        user_id = Column(String(60), Foreign_key('users.id'), nullable=False)
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         text = Column(String(1024), nullable=False)
     else:
         place_id = ""
