3397b79d706ec871042ce8c22d814e32256a1c47
sara-dme <s.daoumy@gmail.com>
2023-11-22 07:24:34 -0800

    task 8 & fix errors task 7

--- models/user.py
+++ models/user.py
@@ -6,14 +6,18 @@ from sqlalchemy.orm import relationship
 from os import getenv
 
 
-class User(BaseModel):
+storage_type = getenv("HBNB_TYPE_STORAGE")
+
+
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
     if storage_type == 'db':
-        email = Comumn(String(128), nullable=False)
+        email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128))
+        places = relationship('Place', cascade="all,delete", backref="user")
     else:
         email = ''
         password = ''
