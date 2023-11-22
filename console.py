e85b6319df8979e5e2e4df8355213652a7e9bdae
aicha652 <aichaazlf652@gmail.com>
2023-11-22 10:08:29 -0800

    fix console

--- console.py
+++ console.py
@@ -139,9 +139,9 @@ class HBNBCommand(cmd.Cmd):
                 setattr(new_instance, key, value)
 
         # new_instance = HBNBCommand.classes[args]()
-        storage.save()
+        new_instance.save()
         print(new_instance.id)
-        storage.save()
+        new_instance.save()
 
     def help_create(self):
         """ Help information for the create method """
