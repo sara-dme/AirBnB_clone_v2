b2350d3b4b44b5e8dc9ad24ef4aeed6618114abc
sara-dme <s.daoumy@gmail.com>
2023-12-28 03:55:44 -0800

    web flask

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" this module start a flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ return hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ display hbnb"""
+    return 'HBNB'
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_c_files(text):
+    """Display the c + value of text var"""
+    return 'C' + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" this module start a flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ return hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ display hbnb"""
+    return 'HBNB'
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_c_files(text):
+    """Display the c + value of text var"""
+    return 'C' + text.replace('_', ' ')
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def display_python(text='is cool'):
+    """ display python followed by the var """
+    return 'Python ' + text.replace('_', ' ')
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def display_number(n):
+    """ display n """
+    return f'{} is a number'
+
+
+@app.route("/numbber_template/<int:n>", strict_slashes=False)
+def display_template(n):
+    """ display template if n is a num """
+    return render_template("5-number.html", n=n)
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def display_even_odd_template(n):
+    """ display html page with num n  """
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
